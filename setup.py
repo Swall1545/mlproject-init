f30e16e508446c5940c19c0724640acf6de2c2e9
s.Wallace <86220042+Swall1545@users.noreply.github.com>
2025-07-18 14:39:26 -0500

    -m "Add setup.py to define installable CLI entry point for mlproject-init script"

---
+++ setup.py
@@ -0,0 +1,18 @@
+#S. Wallace
+# setup.py
+
+from setuptools import setup
+
+# Call setup() function to define this project as a Python package
+setup(
+    name='mlproject-init',  # Install name for package run via CLI
+    version='0.1',  # Initial version
+    py_modules=['mlproject_init'],  # Tells setuptools which .py file to treat as the module
+    install_requires=[],  # Required packages = blank/placeholder
+    entry_points={  # Maps CLI commands to Python functions
+        'console_scripts': [  # Creates a command-line executable
+            'mlproject-init=mlproject_init:init_project_cli'  # Format: command=module:function
+            # When user runs 'mlproject-init' call init_project_cli func. from mlproject_init
+        ],
+    },
+)
